049a8cfe3f6ce60525b7b76f6f52b1b8bee5de9b
Magicfavorite <131023322+Magicfavorite@users.noreply.github.com>
2024-02-14 20:58:54 +0300

    Add files via upload

---
+++ dz 4.12/p3.py
@@ -0,0 +1,11 @@
+meters = float(input("Введите количество метров: "))
+
+kilometers = meters / 1000
+decimeters = meters * 10
+millimeters = meters * 1000
+miles = meters / 1609.34
+
+print("Километры:", kilometers)
+print("Дециметры:", decimeters)
+print("Миллиметры:", millimeters)
+print("Мили:", miles)
---
+++ dz 4.12/p5.py
@@ -0,0 +1,4 @@
+number = input("Введите четырехзначное число: ")
+reversed_number = number[::-1]
+
+print("Перевернутое число:", reversed_number)
